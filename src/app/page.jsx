ee3b76654b013657519beb9b4b9464b3acd549d0
Dannie Vinther <davi@kea.dk>
2024-04-11 22:34:19 +0200

    add popover

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,18 @@
 
 import Details from "@/components/Details";
 
+function Popover({ children, id, type = "auto" }) {
+  return (
+    <div popover={type} id={id}>
+      <button popovertarget={id} popovertargetaction="hide">
+        <span aria-hidden="true">&times;</span>
+        <span className="sr-only">Close Popover</span>
+      </button>
+      {children}
+    </div>
+  );
+}
+
 export default async function Home() {
   return (
     <>
@@ -15,6 +27,18 @@ export default async function Home() {
             animi!
           </p>
         </Details>
+      </section>
+
+      <section>
+        <button popovertarget="popover1">Toggle Popup!</button>
+        <Popover id="popover1">
+          <h2>Popover 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius id
+            enim consectetur sit perspiciatis hic nemo harum officiis laudantium
+            animi!
+          </p>
+        </Popover>
       </section>
     </>
   );
